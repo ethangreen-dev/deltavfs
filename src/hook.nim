6b6daa95c410639278246f01f42632bc6dd127e8
metherul <metherul@protonmail.com>
2021-11-27 07:07:30 -0500

    Large cleanup to hook.nim.

--- src/hook.nim
+++ src/hook.nim
@@ -6,8 +6,13 @@ import hook/hookutils
 import hook/trampoline
 
 import ptr_math
-import winim/core
 import stew/arrayops
+
+from winim/core import
+    VirtualProtect,
+    DWORD,
+    LPVOID,
+    PAGE_EXECUTE_READWRITE
 
 proc makeHook*[T: proc, U: proc](target: T, destination: U): T =
     var targetAddr = cast[ptr byte](target)
@@ -18,46 +23,58 @@ proc makeHook*[T: proc, U: proc](target: T, destination: U): T =
     echo &"target:      {repr(pointer targetAddr)}"
     echo &"destination: {repr(pointer destAddr)}"
 
-    # Create shellcode which jumps into the destination and trampoline.
-    let destJmp = makeJmp(destAddr)
+    # Create shellcode which jumps to the destination. This will be written to the 
+    # start of the target function.
+    # let destJmp = makeJmp(destAddr)
 
     # Determine the number of bytes to steal from the target.
-    let instrSize = toBoundedSize(targetAddr, len(destJmp))
-    echo &"{instrSize} bytes will be removed."
+    let jmpToDestSize = toBoundedSize(targetAddr, JmpSize + sizeof(int))
+    echo &"{jmpToDestSize} bytes will be removed."
 
     # Create the trampoline shellcode -- this will return execution from the cave back to the target.
-    let jumpBack = makeJmp(targetAddr + instrSize)
-    let caveSize = instrSize + len(jumpBack)
+    # let jumpBack = makeJmp(targetAddr + instrSize)
 
-    echo &"JMP to dest: ", toHex(destJmp)
-    echo &"JMP to trgt: ", toHex(jumpBack)
+    # echo &"JMP to dest: ", toHex(destJmp)
+    # echo &"JMP to trgt: ", toHex(jumpBack)
 
     # Search for a code cave in memory to fit the stolen instructions.
-    var trampolineAddr = findCave(targetAddr, caveSize)
-    echo "found cave at ", repr(trampolineAddr)
+    let caveSize = jmpToDestSize + JmpSize
+    let caveAddr = findCave(targetAddr, caveSize)
+
+    echo "found cave at ", repr(caveAddr)
+
+    # Steal bytes from the target function and place into temporary buffer.
+    var stolenBytes = newSeq[byte](jmpToDestSize)
+    copyMem(addr stolenBytes[0], target, jmpToDestSize)
+
+    # Patch RIP-relative branch instructions to reflect new memory offset.
+    updateOffsets(stolenBytes, cast[int](targetAddr), cast[int](caveAddr))
 
     # Enable READ/WRITE access to the code cave.
     var old: DWORD
-    VirtualProtect(cast[LPVOID](trampolineAddr), caveSize, PAGE_EXECUTE_READWRITE, addr(old))
-
-    # Steal bytes at target and place into temporary buffer.
-    var stolenBytes = newSeq[byte](instrSize)
-    copyMem(addr(stolenBytes[0]), target, instrSize)
+    VirtualProtect(cast[LPVOID](caveAddr), caveSize, PAGE_EXECUTE_READWRITE, addr(old))
 
     # Copy bytes from the start of the target function into the trampoline, in addition to the shellcode which jumps back to the target.
-    # iterCopyMem(trampolineAddr, targetAddr, instrSize)
-    iterCopyMem(trampolineAddr, stolenBytes & jumpBack)
+    # iterCopyMem(caveAddr, targetAddr, instrSize)
+    iterCopyMem(caveAddr, stolenBytes & makeJmp(targetAddr + jmpToDestSize))
 
-    # iterCopyMem(trampolineAddr, toOpenArray(cast[ptr UncheckedArray[byte]](targetAddr), 0, instrSize))
-    # iterCopyMem(trampolineAddr + instrSize + 1, jumpBack)
+    echo toHex(stolenBytes & makeJmp(targetAddr))
 
     # Enable write access to the target's memory region.
-    VirtualProtect(targetAddr, instrSize, PAGE_EXECUTE_READWRITE, addr(old))
+    VirtualProtect(targetAddr, jmpToDestSize, PAGE_EXECUTE_READWRITE, addr(old))
 
-    # Create the shellcode to jump execution from the target to the destination.
-    var jumpInto = makeJmp(destAddr)
+    # Write destination jmp and caveAddr to start of target function.
+    iterCopyMem(targetAddr, makeJmp(destAddr) & toByteSeq(cast[int](caveAddr)))
 
-    # Write shellcode to the target function.
-    iterCopyMem(targetAddr, jumpInto)
+    # Steal bytes at target and place into temporary buffer.
+    # var stolenBytes = newSeq[byte](destJmpSize)
+    # copyMem(addr(stolenBytes[0]), target, destJmpSize)
 
-    cast[T](trampolineAddr)
+    # Update RIP-relative addressing in the trampoline.
+    # updateOffsets(stolenBytes, cast[int](targetAddr), cast[int](caveAddr))
+
+    # Create the shellcode to jump execution from the target to the destination. Append the address of the trampoline
+    # so hook functions can execute original behavior without GC pressure.
+    # var jumpInto = makeJmp(destAddr) & toByteSeq(cast[int](caveAddr))
+
+    cast[T](caveAddr)
