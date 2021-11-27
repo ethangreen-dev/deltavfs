45b331144c150a6f3df887555efa86ffd4f4be91
metherul <metherul@protonmail.com>
2021-11-27 07:07:07 -0500

    More experiments with working hook engine.

--- src/deltavfs.nim
+++ src/deltavfs.nim
@@ -1,13 +1,30 @@
+import strutils
+import typetraits
+
 import hook
+import hook/trampoline
+
+import capstone
+
 import deltavfs/injector
 
+import ptr_math
 import winim/core
 
 proc hookMessageBox(hWnd: HWND, lpText: LPCTSTR, lpCaption: LPCSTR, uType: UINT): int = 
-    AllocConsole()
     echo "I GOCHYA BITCH"
 
-    return 1
+    # Call the trampoline function.
+    # let trampoline = getTrampoline(MessageBox)
+    let trampoline = cast[ptr type(MessageBox)](cast[uint](MessageBox) + 14)
+
+    echo toHex(cast[uint](trampoline))
+
+    discard readLine(stdin)
+
+    echo OptOn
+
+    return trampoline[](0, "Hooked!", "Hooked by deltavfs", 0)
 
 proc main() =
     let original = makeHook(MessageBox, hookMessageBox)
@@ -15,7 +32,11 @@ proc main() =
 
     MessageBox(0, "Hello", "HA", 0)
 
-    discard original(0, "Hello world!", "Donkie Kang", 0)
+    # discard original(0, "Hello world!", "Donkie Kang", 0)
+
+    MessageBox(0, "Hello", "HA", 0)
+
+    # discard original(0, "Trying this again...", "Does this actually work?", 0)
 
     # let test = original[](0, "Hello world!", "Donkers", 0)
     # MessageBox(0, "Hello world!", "Donkers", 0)
