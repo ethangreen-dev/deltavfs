808e0a9542deb595193f33a41b254a593cd3d984
metherul <metherul@protonmail.com>
2021-11-30 18:42:48 -0500

    Created hookCreateFileW proc.

--- src/payload.nim
+++ src/payload.nim
@@ -1,40 +1,58 @@
+import os
+import strformat
+
 import hook
 import hook/trampoline
 
 import winim/lean
+import winim/winstr
 
 proc hookMessageBoxA(hWnd: HWND, lpText: LPCTSTR, lpCaption: LPCSTR, uType: UINT): int = 
-    # return MessageBoxW(0, "HOOKED YA BINKO", ":)", 0)
-
     return getTrampoline(hookMessageBoxA)[](0, "HOOKED!", "THOU HATH BEEN HOOKED", 0)
+
+proc hookCreateFileW(
+    lpFileName: LPCWSTR,
+    dwDesiredAccess: DWORD,
+    dwShareMode: DWORD,
+    lpSecurityAttributes: LPSECURITY_ATTRIBUTES,
+    dwCreationDisposition: DWORD,
+    dwFlagsAndAttributes: DWORD,
+    hTemplateFile: HANDLE 
+    ): HANDLE = 
+
+    let newPath = absolutePath("C:/Users/green/Dev/nim/deltavfs/bin/test.txt")
+
+    echo &"[H] CreateFileW with lpFileName: '{$lpFileName}'"
+    echo &"    - Set lpFileName -> '{newPath}'"
+
+    getTrampoline(hookCreateFileW)[](
+        +$newPath,
+        dwDesiredAccess,
+        dwShareMode,
+        lpSecurityAttributes,
+        dwCreationDisposition,
+        dwFlagsAndAttributes,
+        hTemplateFile
+    )
 
 proc mainThread*() {.stdcall, exportc: "mainThread", dynlib.} =
     AllocConsole()
     discard stdout.reopen("CONOUT$", fmWrite)
     discard stderr.reopen("CONOUT$", fmWrite)
 
-    echo "Hello world!"
+    echo &"[::] deltaVFS mainThread start."
 
     # Install hook into MessageBox.
+
+    echo &"[::] Installing hookMessageBoxA"
     discard makeHook(MessageBoxA, hookMessageBoxA)
-    # MessageBoxA(0, "Hello world!", "From nim :)", 0)
-    # echo "Hello world!"
+
+    echo &"[::] Installing hookCreateFileW"
+    discard makeHook(CreateFileW, hookCreateFileW)
+
+    echo &"[::] Initialization complete."
 
 proc testFunction*() {.exportc: "test_function", dynlib.} =
     echo "Calling MessageBoxA"
 
     discard MessageBoxA(0, "Hello!", "World!", 0)
-
-# proc NimMain() {.cdecl, importc.}
-
-# proc DllMain*(hModule: HINSTANCE, reasonForCall: DWORD, lpReserved: LPVOID): WINBOOL {.exportc, dynlib, stdcall.} =
-#     if reasonForCall == DLL_PROCESS_ATTACH:
-#         # DisableThreadLibraryCalls(GetModuleHandle("payload.dll"))
-#         AllocConsole()
-#         discard stdout.reopen("CONOUT$", fmWrite)
-# 
-#         echo "Hello world!"
-#         # NimMain()
-#         # CreateThread(NULL, SIZE_T(0), cast[LPTHREAD_START_ROUTINE](mainThread), cast[LPVOID](hModule), 0, NULL)
-# 
-#     return TRUE
