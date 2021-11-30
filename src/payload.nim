2151ecf3ca89757860e2e063018beac2f15e5ab4
metherul <metherul@protonmail.com>
2021-11-30 02:59:43 -0500

    Custom implementation of LoadLibrary, implemented support for remote-thread payload execution.

--- src/payload.nim
+++ src/payload.nim
@@ -1,18 +1,36 @@
-import threadpool
-
 import hook
 import winim/lean
 
 proc hookMessageBoxA(hWnd: HWND, lpText: LPCTSTR, lpCaption: LPCSTR, uType: UINT): int = 
     return MessageBoxW(0, "HOOKED YA BINKO", ":)", 0)
 
-proc init() =
+proc mainThread*() {.stdcall, exportc: "mainThread", dynlib.} =
+    AllocConsole()
+    discard stdout.reopen("CONOUT$", fmWrite)
+    discard stderr.reopen("CONOUT$", fmWrite)
+
+    echo "Hello world!"
+
     # Install hook into MessageBox.
     discard makeHook(MessageBoxA, hookMessageBoxA)
-    MessageBoxA(0, "Hello world!", "From nim :)", 0)
+    # MessageBoxA(0, "Hello world!", "From nim :)", 0)
+    # echo "Hello world!"
 
-proc main() = 
-    spawn(init())
+proc testFunction*() {.exportc: "test_function", dynlib.} =
+    echo "Calling MessageBoxA"
 
-when isMainModule:
-    main()
+    discard MessageBoxA(0, "Hello!", "World!", 0)
+
+# proc NimMain() {.cdecl, importc.}
+
+# proc DllMain*(hModule: HINSTANCE, reasonForCall: DWORD, lpReserved: LPVOID): WINBOOL {.exportc, dynlib, stdcall.} =
+#     if reasonForCall == DLL_PROCESS_ATTACH:
+#         # DisableThreadLibraryCalls(GetModuleHandle("payload.dll"))
+#         AllocConsole()
+#         discard stdout.reopen("CONOUT$", fmWrite)
+# 
+#         echo "Hello world!"
+#         # NimMain()
+#         # CreateThread(NULL, SIZE_T(0), cast[LPTHREAD_START_ROUTINE](mainThread), cast[LPVOID](hModule), 0, NULL)
+# 
+#     return TRUE
