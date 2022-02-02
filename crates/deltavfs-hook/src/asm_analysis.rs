c7845f89b21ad983791deeffd53285cdad95052b
Ethan Green <egreen4@unca.edu>
2022-02-02 00:03:14 -0500

    Improved `get_bounded_size`.

--- crates/deltavfs-hook/src/asm_analysis.rs
+++ crates/deltavfs-hook/src/asm_analysis.rs
@@ -17,13 +17,16 @@ pub unsafe fn get_bounded_size(target_ptr: *const c_void, desired_size: usize) -
     let mut decoder = Decoder::new(64, target_slice, 0);
 
     // Decode instructions one-by-one until the total bytes read is >= desired_size.
-    Ok(decoder.iter().fold(0, |sum, inst| {
-        if (sum + inst.len()) >= desired_size {
-            return sum + inst.len();
+    let mut sum = 0;
+    for instr in decoder.iter() {
+        if sum >= desired_size {
+            return Ok(sum);
         }
 
-        sum + inst.len()
-    }))
+        sum += instr.len();
+    }
+
+    Err(anyhow!("Failed to determine instruction boundaries."))
 }
 
 pub unsafe fn get_exec_cave(desired_size: usize) -> Result<*const c_void> {
