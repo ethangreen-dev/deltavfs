372c7045e8063ff9eef3600047eaecde283f8981
metherul <metherul@users.noreply.github.com>
2022-02-07 10:28:18 -0500

    Implemented macro to simplify detour ergonomics.

---
+++ crates/macros/src/lib.rs
@@ -0,0 +1,25 @@
+use proc_macro::TokenStream;
+
+use quote::{quote, format_ident};
+use syn::{parse_macro_input, ItemFn};
+
+#[proc_macro_attribute]
+pub fn hook(attr: TokenStream, input: TokenStream) -> TokenStream {
+    let input = parse_macro_input!(input as ItemFn);
+    let ItemFn { attrs, vis, sig, block } = input;
+
+    let stmts = &block.stmts;
+    let args = &sig.inputs;
+    let ret = &sig.output;
+
+    let quoted = quote! {
+        #(#attrs)* #vis #sig {
+            type RecallType = fn(#args) #ret;
+            let recall: RecallType = std::mem::transmute::<usize, RecallType>(*TEST_RECALL.get().unwrap());
+
+            #(#stmts)*
+        }
+    };
+
+    TokenStream::from(quoted)
+}
