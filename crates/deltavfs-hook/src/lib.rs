90bed13ef56d52026018f168e24f3a6d8f9dfb5f
Ethan Green <egreen4@unca.edu>
2022-02-16 02:39:27 -0500

    Improved hook ergonomics, implemented additional detours.

--- crates/deltavfs-hook/src/lib.rs
+++ crates/deltavfs-hook/src/lib.rs
@@ -15,22 +15,27 @@ use windows::Win32::{
         FILE_ACCESS_FLAGS, FILE_CREATION_DISPOSITION, FILE_FLAGS_AND_ATTRIBUTES, FILE_SHARE_MODE,
     },
 };
+use windows::Win32::Foundation::SIZE;
 
-static TEST_RECALL: OnceCell<usize> = OnceCell::new();
+// static TEST_RECALL: OnceCell<usize> = OnceCell::new();
 
 #[no_mangle]
 unsafe extern "stdcall" fn hook_init() {
     println!("Hook initialization.");
 
-    let target_addr = pe::get_func_addr("kernel32", "CreateFileW").unwrap();
-    let recall = hook::install_hook(target_addr, test as _).unwrap();
+    // let target_addr = pe::get_func_addr("kernel32", "CreateFileW").unwrap();
+    // let recall = hook::install_hook(target_addr, test as _).unwrap();
 
-    println!("Got recall value of {:?}", recall);
+    // println!("Got recall value of {:?}", recall);
 
-    TEST_RECALL.set(recall as _).unwrap();
+    // TEST_RECALL.set(recall as _).unwrap();
+
+    test_init();
+    map_view_of_file_init();
+    create_file_mapping_init();
 }
 
-#[hook]
+#[hook("kernel32", "CreateFileW")]
 unsafe fn test(
     file_name: PWSTR,
     desired_access: FILE_ACCESS_FLAGS,
@@ -41,7 +46,7 @@ unsafe fn test(
     template_file: HANDLE,
 ) -> HANDLE {
     let thing = WideCString::from_ptr_str(file_name.0).to_string().unwrap();
-    println!("{}", thing);
+    println!("[CreateFileW] {}", thing);
 
     let handle = recall(
         file_name,
@@ -54,4 +59,46 @@ unsafe fn test(
     );
 
     handle
+}
+
+#[hook("kernel32", "MapViewOfFile")]
+unsafe fn map_view_of_file(
+    file_mapping_object: HANDLE,
+    desired_access: u32,
+    file_offset_high: u32,
+    file_offset_low: u32,
+    number_of_bytes_to_map: usize
+) -> *const usize {
+    let ptr = recall(
+        file_mapping_object,
+        desired_access,
+        file_offset_high,
+        file_offset_low,
+        number_of_bytes_to_map
+    );
+
+    println!("[MapViewOfFile] with {} bytes to map. created ptr: {:x?}", number_of_bytes_to_map, ptr);
+
+    ptr
+}
+
+#[hook("kernel32", "CreateFileMappingW")]
+unsafe fn create_file_mapping(
+    file: HANDLE,
+    attributes: *const SECURITY_ATTRIBUTES,
+    protect: u32,
+    max_size_high: u32,
+    max_size_low: u32,
+    name: *const char
+) -> HANDLE {
+    println!("[CreateFileMapping] with max_size_high: {}, max_size_low: {}", max_size_high, max_size_low);
+
+    recall(
+        file,
+        attributes,
+        protect,
+        max_size_high,
+        max_size_low,
+        name
+    )
 }
--- crates/macros/src/lib.rs
+++ crates/macros/src/lib.rs
@@ -1,11 +1,13 @@
 use proc_macro::TokenStream;
 
-use quote::quote;
-use syn::{parse_macro_input, ItemFn};
+use quote::{format_ident, quote};
+use syn::{parse_macro_input, ItemFn, AttributeArgs};
 
 #[proc_macro_attribute]
-pub fn hook(_: TokenStream, input: TokenStream) -> TokenStream {
+pub fn hook(args: TokenStream, input: TokenStream) -> TokenStream {
     let input = parse_macro_input!(input as ItemFn);
+    let args = parse_macro_input!(args as AttributeArgs);
+
     let ItemFn {
         attrs,
         vis,
@@ -13,16 +15,34 @@ pub fn hook(_: TokenStream, input: TokenStream) -> TokenStream {
         block,
     } = input;
 
+    let module = args.get(0).expect("Expected module name, got nothing.");
+    let func = args.get(1).expect("Expected function name, got nothing.");
+
     let stmts = &block.stmts;
     let args = &sig.inputs;
     let ret = &sig.output;
+    let name = &sig.ident;
+
+    let init_name = format_ident!("{}_init", &sig.ident);
+    let recall_name = format_ident!("{}_RECALL", &sig.ident);
 
     let quoted = quote! {
         #(#attrs)* #vis #sig {
             type RecallType = fn(#args) #ret;
-            let recall: RecallType = std::mem::transmute::<usize, RecallType>(*TEST_RECALL.get().unwrap());
+            let recall: RecallType = std::mem::transmute::<usize, RecallType>(*#recall_name.get().unwrap());
 
             #(#stmts)*
+        }
+
+        static #recall_name: OnceCell<usize> = OnceCell::new();
+
+        fn #init_name() {
+            unsafe {
+                let target_addr = pe::get_func_addr(#module, #func).unwrap();
+                let recall = hook::install_hook(target_addr, #name as _).unwrap();
+
+                #recall_name.set(recall as _).expect("Failed to set recall value for #recall_name.")
+            }
         }
     };
 
