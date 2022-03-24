98a5514fbbd5d683a86e032df9f4e494385a554d
Ethan Green <egreen4@unca.edu>
2022-03-23 22:40:47 -0400

    Simple CLI interface with `--id` and `--path` arguments.

--- crates/deltavfs-injector/src/main.rs
+++ crates/deltavfs-injector/src/main.rs
@@ -1,13 +1,28 @@
 mod injector;
 
+use std::env;
+use std::str::FromStr;
+
 fn main() {
     unsafe {
+        let proc_handle = match env::args().nth(1).unwrap().as_str() {
+            "--id" => {
+                injector::open_process(env::args().nth(2).unwrap().parse::<u32>().unwrap())
+            },
+            "--path" => {
+                let path = env::args().nth(2).unwrap();
+
+                injector::start_process(path.as_str(), &["test"])
+            },
+            _ => panic!("Incorrect argument.")
+        }.unwrap();
+
         //let path = "notepad.exe";
         // let path = r#"C:\Program Files (x86)\Steam\steamapps\common\Factorio\bin\x64\factorio.exe"#;
 
         // let path = r#"C:\Users\green\Downloads\Enter.the.Gungeon.v1.1.3.Hotfix.3\EtG.exe"#;
         let path = r#"C:\GOG Games\Factorio\bin\x64\factorio.exe"#;
 
-        injector::inject_into(path, &["steam://rungameid/311690"]).unwrap();
+        injector::inject_into(proc_handle.clone()).unwrap();
     }
 }
