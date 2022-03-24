2449c61e8c733750d6db443adbbd7041aa3ef49f
Ethan Green <egreen4@unca.edu>
2022-03-23 22:45:48 -0400

    Implemented `Echo` and `TryFrom` to enable `u8 -> Magic` casting.

--- crates/shared/src/ipc/models.rs
+++ crates/shared/src/ipc/models.rs
@@ -1,3 +1,4 @@
+use anyhow::anyhow;
 use rkyv::{
     Archive,
     Serialize,
@@ -5,16 +6,41 @@ use rkyv::{
 };
 
 #[repr(u8)]
-pub enum Request {
-    ResolvePath = 0
+#[derive(Debug)]
+pub enum Magic {
+    Msg = 0,
+    ResolvePath = 1,
+    RegisterClientResponse = 254,
+    RegisterClientRequest = 255
 }
 
-#[derive(Archive, Serialize, Deserialize)]
-pub struct ResolvePathRequest {
-    pub path: String
+impl TryFrom<u8> for Magic {
+    type Error = anyhow::Error;
+
+    fn try_from(value: u8) -> Result<Self, Self::Error> {
+        match value {
+            0 => Ok(Magic::Msg),
+            1 => Ok(Magic::ResolvePath),
+            254 => Ok(Magic::RegisterClientResponse),
+            255 => Ok(Magic::RegisterClientRequest),
+            _ => Err(anyhow!("Invalid magic header value."))
+        }
+    }
 }
 
 #[derive(Archive, Serialize, Deserialize)]
 pub struct Echo {
+    pub client_id: u16,
     pub msg: String
+}
+
+#[derive(Archive, Serialize, Deserialize)]
+pub struct ResolvePathRequest {
+    pub client_id: u16,
+    pub path: String
+}
+
+#[derive(Archive, Serialize, Deserialize)]
+pub struct RegisterClientResponse {
+    pub client_id: u16
 }
